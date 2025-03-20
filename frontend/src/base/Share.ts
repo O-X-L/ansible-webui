e9def36a150687157822a4b8d7de2e18fc4cbd94
Rath Pascal <rath@oxl.at>
2025-03-20 15:24:03 +0100

    fix: job-execution prompt-validation (#4)

---
+++ frontend/src/base/Share.ts
@@ -0,0 +1,24 @@
+import { writable } from 'svelte/store';
+
+interface backendInfos {
+    authenticated: boolean,
+    sso: boolean,
+    user: null|string,
+    user_id: null|number,
+    version: string,
+    logo: string,
+}
+
+interface shareObject {
+    backend: backendInfos,
+    lang: any,
+    updateInterval: number,
+}
+
+export const share = writable({
+    backend: {},
+    lang: {},
+    updateInterval: 500,
+} as shareObject);
+
+export const urlParams = new URLSearchParams(window.location.search);
