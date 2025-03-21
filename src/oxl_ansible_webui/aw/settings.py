a2c21f479996e38aa212c940aca6b0a100826437
Rath Pascal <rath@oxl.at>
2025-03-21 21:13:18 +0100

    feat: prepare for new docker-build (#4)

--- src/oxl_ansible_webui/aw/settings.py
+++ src/oxl_ansible_webui/aw/settings.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_auto_logout.middleware.auto_logout',
     'aw.apps.AwMiddleware',
 ]
 
