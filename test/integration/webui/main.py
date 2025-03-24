dcd4ac2fff6dc53c90389d857b0be8cc66de3058
Rath Pascal <rath@oxl.at>
2025-03-24 23:57:58 +0100

    fix: update web-frontend integration-tests (#4)

--- test/integration/webui/main.py
+++ test/integration/webui/main.py
@@ -30,7 +30,9 @@ def _get_requests(url: str) -> dict:
 
     for r in DRIVER.requests:
         if not r.response or r.url.endswith('favicon.ico') or \
-                r.url.find('google') != -1 or r.url.find('chrome_component') != -1:
+                r.url.find('google') != -1 or \
+                r.url.find('chrome_component') != -1 or \
+                r.url.find('chromewebstore') != -1:
             continue
 
         print(f" > {r.url.replace(BASE_URL, '')}")
