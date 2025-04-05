3dae4cb955c245e53ea0407dbdfd92e42384d754
Rath Pascal <rath@oxl.at>
2025-04-06 00:06:12 +0200

    fix: add basic tests for mariadb/mysql-db (#24)

---
+++ scripts/test_db_psql.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# docker run --detach --name aw-psql --env POSTGRES_USER=aw --env POSTGRES_PASSWORD=test --env POSTGRES_DB=aw -p 5432:5432 postgres:latest
+
+set -e
+
+cd "$(dirname "$0")/.."
+
+echo ''
+echo 'INTEGRATION TESTS DB POSTGRESQL'
+echo ''
+
+export AW_DB_TYPE='psql'
+export AW_DB='aw'
+export AW_DB_HOST="${AW_DB_HOST:-127.0.0.1}"
+export AW_DB_PORT='5432'
+export AW_DB_USER='aw'
+export AW_DB_PWD='test'
+
+source ./scripts/test_db_base.sh
+
+success
