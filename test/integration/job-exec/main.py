df68336139f6fa9fcf2045555290a125feca473a
Rath Pascal <rath@oxl.at>
2025-10-27 18:51:24 +0100

    test: fix & extend job-execution tests (#78)

--- test/integration/job-exec/main.py
+++ test/integration/job-exec/main.py
@@ -64,6 +64,17 @@ def _check_status_until_finished_or_timeout(job_id: int, exec_count: int):
             continue
 
         print(' =>', e)
+
+        # verbose output for troubleshooting
+        if e['failed']:
+            for log_kind, log_key in {'STDOUT': 'log_stdout', 'STDERR': 'log_stderr'}.items():
+                if e[log_key] is None or not Path(e[log_key]).is_file():
+                    continue
+
+                with open(e[log_key], 'r', encoding='utf-8') as f:
+                    print(f' => {log_kind}:')
+                    print(f.read())
+
         return e
 
     print(' =>', e)
@@ -200,16 +211,21 @@ def test_creds(jid: int = 3):
 
 
 def test_repo_git(jid: int = 4):
+    path_repo_base = '/tmp/ansible-webui/repositories'
+
     print('\nREPO GIT | ADD REPO')
     repo = f'myRepo-{int(time())}'
     origin = 'https://github.com/O-X-L/ansible-webui.git'
     branch = 'latest'
+    repo_id = 1
+    path_repo = f'{path_repo_base}/{repo_id}_{repo}'
+
     _api_request(
         'repository',
         'post',
         {
             'name': repo, 'rtype': 2, 'git_origin': origin, 'git_branch': branch, 'git_playbook_base': 'test',
-            'isolate': False,
+            'git_isolate': False,
         }
     )
 
@@ -219,18 +235,18 @@ def test_repo_git(jid: int = 4):
         'post',
         {
             'name': f'job{jid}', 'playbook_file': 'play1.yml', 'inventory_file': 'inv/empty.yml',
-            'repository': 1,
+            'repository': repo_id,
         }
     )
 
-    print('REPO GIT | EXECUTE (not isolated)')
-    rmtree(f'/tmp/ansible-webui/repositories/{repo}', ignore_errors=True)  # clean auto-downloaded repo
+    print('REPO GIT | EXECUTE')
+    rmtree(path_repo, ignore_errors=True)  # clean pre-existing repo
     _api_request(
         f'job/{jid}',
         'post',
     )
 
-    print('REPO GIT | CHECK (not isolated)')
+    print('REPO GIT | CHECK')
     e = _check_status_until_finished_or_timeout(jid, 1)
 
     assert e['status_name'] == 'Finished'
@@ -240,9 +256,51 @@ def test_repo_git(jid: int = 4):
 
     with open(e['log_stdout_repo'], 'r', encoding='utf-8') as f:
         log = f.read()
-        assert log.find(f'git clone --branch {branch} {origin} /tmp/ansible-webui/repositories/{repo}') != -1
+        assert log.find(f'git clone --branch {branch} {origin} {path_repo}') != -1
 
-    # todo: execute with isolation
+    # ISOLATED
+    repo = f'myRepo-{int(time())}'
+    repo_id = 2
+    exec_id = e['id'] + 1
+    path_repo = f"{path_repo_base}/isolated/{repo_id}_{repo}/{exec_id}"
+
+    _api_request(
+        'repository',
+        'post',
+        {
+            'name': repo, 'rtype': 2, 'git_origin': origin, 'git_branch': branch, 'git_playbook_base': 'test',
+            'git_isolate': True,
+        }
+    )
+
+    print('REPO GIT-ISOLATED | UPDATE JOB')
+    _api_request(
+        f'job/{jid}',
+        'put',
+        {
+            'name': f'job{jid}', 'playbook_file': 'play1.yml', 'inventory_file': 'inv/empty.yml',
+            'repository': repo_id,
+        }
+    )
+
+    print('REPO GIT-ISOLATED | EXECUTE')
+    rmtree(path_repo, ignore_errors=True)  # clean pre-existing repo
+    _api_request(
+        f'job/{jid}',
+        'post',
+    )
+
+    print('REPO GIT-ISOLATED | CHECK')
+    e = _check_status_until_finished_or_timeout(jid, 2)
+
+    assert e['status_name'] == 'Finished'
+    assert e['failed'] is False
+    assert e['log_stdout_repo'] is not None
+    assert Path(e['log_stdout_repo']).is_file()
+
+    with open(e['log_stdout_repo'], 'r', encoding='utf-8') as f:
+        log = f.read()
+        assert log.find(f"git clone --branch {branch} {origin} {path_repo}") != -1
 
 
 def test_execution_cleanup(jid: int = 1, exec_id: int = 1):
